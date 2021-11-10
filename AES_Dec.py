7b731cb048a915f8202b8e159947a0c6c1cdb033
baiqyustiarini <baiqyustiarini@gmail.com>
2021-11-10 07:46:23 +0700

    Subscriber

---
+++ AES_Dec.py
@@ -0,0 +1,56 @@
+from Cryptodome.Cipher import AES
+import binascii
+from time import sleep
+import paho.mqtt.client as mqtt
+import ast
+# import json
+
+#MQTT
+mqttBroker = "192.168.43.57"
+client = mqtt.Client("AES Subscriber")
+client.connect(mqttBroker)
+
+def add_to_16(text):
+    while len(text) % 16 != 0:
+        text += '\0'
+    return text
+
+
+def encrypt(data, password):
+    if isinstance(password, str):
+        password = password.encode('utf8')
+
+    bs = AES.block_size
+    pad = lambda s: s + (bs - len(s) % bs) * chr(bs - len(s) % bs)
+    cipher = AES.new(password, AES.MODE_ECB)
+    data = cipher.encrypt(pad(data).encode('utf8'))
+    encrypt_data = binascii.b2a_hex (data) # Output HEX
+         # Encrypt_data = Base64.b64encode (data) #Climinate Comments, Output Base64 Format
+    return encrypt_data.decode('utf8')
+
+
+def decrypt(decrData, password):
+    if isinstance(password, str):
+        password = password.encode('utf8')
+    cipher = AES.new(password, AES.MODE_ECB)
+    plain_text = cipher.decrypt(binascii.a2b_hex(decrData))
+    return plain_text.decode('utf8').rstrip('\0')
+
+
+if __name__ == '__main__':
+        Password = input("Password: ")
+        password = add_to_16(Password)
+        def on_message(client, userdata, message):
+            msg = message.payload.decode("utf-8")
+            msgg = ast.literal_eval(str(msg))
+            # msgg = json.loads(str(msg))
+            decrypted = decrypt(msgg, Password)
+            print("Decrypted\t: ", bytes.decode(decrypted))
+
+            # sleep(3)
+
+        client.loop_start()
+        client.subscribe("AES")
+        client.on_message=on_message
+        sleep(300)
+        client.loop_stop
