0f8859af6116367257d8a5c83d593d40f87a060a
Deny Kurniawan <dekakrens@gmail.com>
2021-11-12 23:13:53 +0700

    AES decrypt fixed

--- AES_Dec.py
+++ AES_Dec.py
@@ -4,7 +4,6 @@ from time import sleep
 import paho.mqtt.client as mqtt
 import ast
 from decouple import config
-# import json
 
 #MQTT
 mqttBroker = config('ADDRESS')
@@ -13,14 +12,12 @@ client.connect(mqttBroker)
 
 def add_to_16(text):
     while len(text) % 16 != 0:
-        text += '\0'
+        text += ' '
     return text
-
 
 def encrypt(data, password):
     if isinstance(password, str):
         password = password.encode('utf8')
-
     bs = AES.block_size
     pad = lambda s: s + (bs - len(s) % bs) * chr(bs - len(s) % bs)
     cipher = AES.new(password, AES.MODE_ECB)
@@ -29,7 +26,6 @@ def encrypt(data, password):
          # Encrypt_data = Base64.b64encode (data) #Climinate Comments, Output Base64 Format
     return encrypt_data.decode('utf8')
 
-
 def decrypt(decrData, password):
     if isinstance(password, str):
         password = password.encode('utf8')
@@ -37,21 +33,17 @@ def decrypt(decrData, password):
     plain_text = cipher.decrypt(binascii.a2b_hex(decrData))
     return plain_text.decode('utf8').rstrip('\0')
 
-
 if __name__ == '__main__':
-        Password = input("Password: ")
-        password = add_to_16(Password)
-        def on_message(client, userdata, message):
-            msg = message.payload.decode("utf-8")
-            msgg = ast.literal_eval(str(msg))
-            # msgg = json.loads(str(msg))
-            decrypted = decrypt(msgg, Password)
-            print("Decrypted\t: ", bytes.decode(decrypted))
+    Password = input("Password: ")
+    password = add_to_16(Password)
+    def on_message(client, userdata, message):
+        msg = message.payload.decode("utf-8")
+        decrypted = decrypt(msg, password)
+        print("Decrypted\t: ", decrypted[0:15])
 
-            # sleep(3)
 
-        client.loop_start()
-        client.subscribe("AES")
-        client.on_message=on_message
-        sleep(300)
-        client.loop_stop
+    client.loop_start()
+    client.subscribe("AES")
+    client.on_message=on_message
+    sleep(300)
+    client.loop_stop
