b2f84b3dc5034962722a5ef45166189946597d03
baiqyuniar <baiqyustiarini@gmail.com>
2022-02-08 13:17:34 +0700

    speck

--- Speck_Dec.py
+++ Speck_Dec.py
@@ -193,11 +193,11 @@ def pencatatan(msg, dateSend):
 	f.write(msg + ";" + now + ";" + dateSend + "\n")
 
 if __name__ == "__main__":
-   # key = 0x1f1e1d1c1b1a19181716151413121110 
+    key = 0x1f1e1d1c1b1a19181716151413121110 
    # key = 0x1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a0908
-    key = 0x1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100
-    cipher = SpeckCipher(key, 256, 128, 'ECB')
-   # cipher = SpeckCipher(key, 256, 128, 'CBC', 0x123456789ABCDEF0)
+   # key = 0x1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100
+    cipher = SpeckCipher(key, 128, 128, 'ECB')
+   # cipher = SpeckCipher(key, 128, 128, 'CBC', 0x123456789ABCDEF0)
     def on_message(client, userdata, message):
         raw = json.loads(message.payload.decode("utf-8"))
         msg = raw['cipher']
